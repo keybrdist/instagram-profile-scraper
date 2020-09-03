aea632962936edc529d45503ebd2f4756ab949cf
Michael Ramirez <keybrdist@users.noreply.github.com>
2020-09-03 04:40:42 -0600

    Update getProfileInfo.js

--- examples/getProfileInfo.js
+++ examples/getProfileInfo.js
@@ -1,7 +1,7 @@
-var bandcamp = require('../lib/index');
+var client = require('../lib/index');
 
 var profileUrl = 'http://instagram.com/dnbradio';
-bandcamp.getProfileInfo(profileUrl, function(error, profileInfo) {
+client.getProfileInfo(profileUrl, function(error, profileInfo) {
   if (error) {
     console.log(error);
   } else {
